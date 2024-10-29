2aac38f51c6539926a29cfc311dac9888e731baa
onurozkinaci@gmail.com <onurozkinaci@gmail.com>
2024-10-29 18:48:45 +0300

    RegisterController is created and config structure is updated for Json Web Token(JWT) integration.

--- IdentityServer/Multishop.IdentityServer/Config.cs
+++ IdentityServer/Multishop.IdentityServer/Config.cs
@@ -2,57 +2,69 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 
 namespace Multishop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("ResourceCatalog"){Scopes={"CatalogFullPermission","CatalogReadPermission"}},
+            new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermission"}},
+            new ApiResource("ResourceOrder"){Scopes={"OrderFullPermission"}},
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+        };
+        //=>Tokeni alinan kullanici icin tokendaki hangi bilgilerin kullanilacagi belirtildi;
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+       {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+       };
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
+            new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
+            new ApiScope("DiscountFullPermission","Full authority for discount operations"),
+            new ApiScope("OrderFullPermission","Full authority for order operations")
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        { 
+            //Visitor
+            new Client
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
+               ClientId = "MultishopVisitorId",
+               ClientName = "Multi Shop Visitor User",
+               AllowedGrantTypes = GrantTypes.ClientCredentials,
+               ClientSecrets = {new Secret("multishopsecret".Sha256())},
+               AllowedScopes = { "CatalogReadPermission" }
+            },
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
+            //Manager
+            new Client
             {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+               ClientId = "MultishopManagerId",
+               ClientName = "Multi Shop Manager User",
+               AllowedGrantTypes = GrantTypes.ClientCredentials,
+               ClientSecrets = {new Secret("multishopsecret".Sha256())},
+               AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission" }
+            },
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
+            //Admin
+            new Client
+            {
+               ClientId = "MultishopAdminId",
+               ClientName = "Multi Shop Admin User",
+               AllowedGrantTypes = GrantTypes.ClientCredentials,
+               ClientSecrets = {new Secret("multishopsecret".Sha256())},
+               AllowedScopes = { "CatalogFullPermission","CatalogReadPermission", "DiscountFullPermission", "OrderFullPermission", IdentityServerConstants.LocalApi.ScopeName, IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile},
+               AccessTokenLifetime=600 //10 minutes
+            },
+        };
     }
 }
---
+++ IdentityServer/Multishop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multishop.IdentityServer.Dtos;
+using Multishop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace Multishop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var value = new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname
+            };
+            var result = await _userManager.CreateAsync(value, userRegisterDto.Password);
+            if (result.Succeeded)
+                return Ok("User is added successfully.");
+            else
+                return Ok("An error occured, please try again!");
+        }
+    }
+}
